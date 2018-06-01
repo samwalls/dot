2b1a043137c49ba98616d2559330d34b1060d159
samwalls <sam@s-w.io>
2018-06-01 16:13:16 +0100

    move melpa include to top

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,15 @@
+;; MELPA
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+;; END MELPA
+
 ;; SUBLIMITY
 ;;(add-to-list 'load-path "~/.dot/emacs/packages/sublimity/")
 ;;(require 'sublimity)
@@ -65,15 +77,3 @@
 (load-theme 'carloscuesta-material)
 
 ;; END THEMES
-
-;; MELPA
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-  (add-to-list 'package-archives (cons "melpa" url) t))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize)
-;; END MELPA
