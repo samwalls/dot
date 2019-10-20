6fbf6c981e3cf8c5d3426e9f45115c649f2eb9a7
samwalls <sam@s-w.io>
2019-10-20 16:42:14 +0100

    fix gfxcpu driver switching script

--- bin/sh/gfxcpu.sh
+++ bin/sh/gfxcpu.sh
@@ -2,6 +2,7 @@
 
 echo "enabling bumblebee daemon"
 systemctl enable bumblebeed
+systemctl start bumblebeed
 
 echo "switching graphics driver (CPU hybrid graphics driver)"
 optimus-manager --switch intel
