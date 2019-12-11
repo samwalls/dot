2e72b7f6e8c5c8ad82ba4312089afe6934495a33
Sam Walls <sam@s-w.io>
2019-12-11 14:26:30 +0000

    update aur-wrapper in install_spacemacs.sh

--- install_spacemacs.sh
+++ install_spacemacs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # TODO: separate software installation from configuration
+# TODO: define dotfiles aur-manager somewhere common (e.g. might not use yay in the future)
 
-pacman -Sy emacs
+yay -Sy emacs
 
 # LSP layers and various others are currently only available on the develop branch of spacemacs
 git clone --single-branch --branch develop https://github.com/syl20bnr/spacemacs ${HOME}/.emacs.d
