1b5d65c2a76423d5cb5183846ba6f2d1e0c226fe
Sam Walls <sam@s-w.io>
2020-03-05 15:12:41 +0000

    add git status column to exa aliases

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -1,4 +1,6 @@
 # these aliases depend on `$DOTFILE_PATH/zsh/zsh.d/environment.zsh` as well as `$DOTFILE_PATH/install_various.sh`
 alias ls="exa"
+alias ll="ls -lh --git"
+alias l="ll -a"
 alias grep="rg"
 alias cat="bat --paging=never"
