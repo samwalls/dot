ae94bfa384e8e635f1a6ba07491651ae8cb0c628
Sam Walls <sam@s-w.io>
2020-07-31 11:15:09 +0100

    add default .i3status.conf + installation

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -28,6 +28,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cautious_install "${HOME}/.zshrc" "${DIR}/zsh/.zshrc"
 cautious_install "${HOME}/.i3" "${DIR}/.i3"
+cautious_install "${HOME}/.i3status.conf" "${DIR}/.i3status.conf"
 cautious_install "${HOME}/.xinitrc" "${DIR}/.xinitrc"
 cautious_install "${HOME}/.Xmodmap" "${DIR}/.Xmodmap"
 cautious_install "${HOME}/.Xresources" "${DIR}/.Xresources"
